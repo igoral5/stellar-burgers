ae3be846d2bc2c84bef5129c1b47ba0fbc18effa
Igor Alimov <ilalimov@mts.ru>
2026-06-18 21:53:26 +0300

    Complete test

--- tests/stellar-burger.spec.ts
+++ tests/stellar-burger.spec.ts
@@ -137,8 +137,12 @@ test('Order a burger using access token', async ({ context, page }) => {
   await expect(page.getByTestId('order-success')).toBeVisible({
     timeout: 20000
   });
+  await expect(page.getByTestId('order-success')).toHaveText('106657');
   await page.keyboard.press('Escape');
   await expect(page.getByTestId('order-success')).not.toBeVisible();
+  await expect(
+    page.getByTestId('burger-constructor-element')
+  ).not.toBeVisible();
 });
 
 test('Order a burger using refresh token', async ({ context, page }) => {
@@ -232,8 +236,12 @@ test('Order a burger using refresh token', async ({ context, page }) => {
   await expect(page.getByTestId('order-success')).toBeVisible({
     timeout: 20000
   });
+  await expect(page.getByTestId('order-success')).toHaveText('106657');
   await page.keyboard.press('Escape');
   await expect(page.getByTestId('order-success')).not.toBeVisible();
+  await expect(
+    page.getByTestId('burger-constructor-element')
+  ).not.toBeVisible();
 });
 
 test('Order a burger using manual login', async ({ page }) => {
@@ -316,6 +324,221 @@ test('Order a burger using manual login', async ({ page }) => {
   await expect(page.getByTestId('order-success')).toBeVisible({
     timeout: 20000
   });
+  await expect(page.getByTestId('order-success')).toHaveText('106657');
   await page.keyboard.press('Escape');
   await expect(page.getByTestId('order-success')).not.toBeVisible();
+  await expect(
+    page.getByTestId('burger-constructor-element')
+  ).not.toBeVisible();
+});
+
+test('Close modal by press Esc', async ({ page }) => {
+  await page.routeFromHAR('./tests/hars/ingredients.har', {
+    url: '**/api/ingredients',
+    update: false
+  });
+
+  await page.route('**/api/auth/user', async (route) => {
+    await route.fulfill({
+      status: 401,
+      json: { success: false, message: 'You should be authorised' }
+    });
+  });
+
+  await page.goto('/');
+  await expect(page.getByTestId('ingredients')).toBeVisible();
+  await page
+    .getByRole('listitem')
+    .filter({ hasText: 'Биокотлета из марсианской Магнолии' })
+    .getByRole('link')
+    .click();
+  await expect(page.getByTestId('modal')).toBeVisible();
+  await page.keyboard.press('Escape');
+  await expect(page.getByTestId('modal')).not.toBeVisible();
+});
+
+test('Close modal by click button', async ({ page }) => {
+  await page.routeFromHAR('./tests/hars/ingredients.har', {
+    url: '**/api/ingredients',
+    update: false
+  });
+
+  await page.route('**/api/auth/user', async (route) => {
+    await route.fulfill({
+      status: 401,
+      json: { success: false, message: 'You should be authorised' }
+    });
+  });
+
+  await page.goto('/');
+  await expect(page.getByTestId('ingredients')).toBeVisible();
+  await page
+    .getByRole('listitem')
+    .filter({ hasText: 'Биокотлета из марсианской Магнолии' })
+    .getByRole('link')
+    .click();
+  await expect(page.getByTestId('modal')).toBeVisible();
+  await page.getByRole('button').filter({ hasText: /^$/ }).click();
+  await expect(page.getByTestId('modal')).not.toBeVisible();
+});
+
+test('Close modal by click modal overlay', async ({ page }) => {
+  await page.routeFromHAR('./tests/hars/ingredients.har', {
+    url: '**/api/ingredients',
+    update: false
+  });
+
+  await page.route('**/api/auth/user', async (route) => {
+    await route.fulfill({
+      status: 401,
+      json: { success: false, message: 'You should be authorised' }
+    });
+  });
+
+  await page.goto('/');
+  await expect(page.getByTestId('ingredients')).toBeVisible();
+  await page
+    .getByRole('listitem')
+    .filter({ hasText: 'Биокотлета из марсианской Магнолии' })
+    .getByRole('link')
+    .click();
+  await expect(page.getByTestId('modal')).toBeVisible();
+  await page.getByTestId('modal-overlay').click({ position: { x: 10, y: 10 } });
+  await expect(page.getByTestId('modal')).not.toBeVisible();
+});
+
+test('Register new user', async ({ page }) => {
+  await page.routeFromHAR('./tests/hars/ingredients.har', {
+    url: '**/api/ingredients',
+    update: false
+  });
+
+  await page.route('**/api/auth/user', async (route) => {
+    const request = route.request();
+
+    const headers = await request.allHeaders();
+
+    if (
+      'authorization' in headers &&
+      headers['authorization'] === 'Bearer accessToken'
+    ) {
+      await route.fulfill({
+        status: 200,
+        json: {
+          success: true,
+          user: {
+            email: 'user@example.com',
+            name: 'John Doe'
+          }
+        }
+      });
+    } else {
+      await route.fulfill({
+        status: 401,
+        json: { success: false, message: 'You should be authorised' }
+      });
+    }
+  });
+
+  await page.route('**/api/auth/register', async (route) => {
+    await route.fulfill({
+      status: 200,
+      json: {
+        success: true,
+        accessToken: 'Bearer accessToken',
+        refreshToken: 'refreshToken',
+        user: {
+          email: 'user@example.com',
+          name: 'John Doe'
+        }
+      }
+    });
+  });
+
+  await page.goto('/');
+  await expect(page.getByTestId('ingredients')).toBeVisible();
+  await page.getByRole('link', { name: 'Личный кабинет' }).click();
+  await page.getByRole('link', { name: 'Зарегистрироваться' }).click();
+  await page.locator('input[name="name"]').fill('John Doe');
+  await page.locator('input[name="email"]').fill('user@example.com');
+  await page.locator('input[name="password"]').fill('secretpassword');
+  await page.getByRole('button', { name: 'Зарегистрироваться' }).click();
+  await expect(page.getByRole('link', { name: 'John Doe' })).toBeVisible();
+});
+
+test('Forgot password', async ({ page }) => {
+  await page.routeFromHAR('./tests/hars/ingredients.har', {
+    url: '**/api/ingredients',
+    update: false
+  });
+
+  await page.route('**/api/auth/user', async (route) => {
+    const request = route.request();
+
+    const headers = await request.allHeaders();
+
+    if (
+      'authorization' in headers &&
+      headers['authorization'] === 'Bearer accessToken'
+    ) {
+      await route.fulfill({
+        status: 200,
+        json: {
+          success: true,
+          user: {
+            email: 'user@example.com',
+            name: 'John Doe'
+          }
+        }
+      });
+    } else {
+      await route.fulfill({
+        status: 401,
+        json: { success: false, message: 'You should be authorised' }
+      });
+    }
+  });
+
+  await page.route('**/api/password-reset', async (route) => {
+    await route.fulfill({
+      status: 200,
+      json: { success: true, message: 'Reset email sent' }
+    });
+  });
+
+  await page.route('**/api/password-reset/reset', async (route) => {
+    await route.fulfill({
+      status: 200,
+      json: { success: true, message: 'Password successfully reset' }
+    });
+  });
+
+  await page.route('**/api/auth/login', async (route) => {
+    await route.fulfill({
+      status: 200,
+      json: {
+        success: true,
+        accessToken: 'Bearer accessToken',
+        refreshToken: 'refreshToken',
+        user: {
+          email: 'user@example.com',
+          name: 'John Doe'
+        }
+      }
+    });
+  });
+
+  await page.goto('/');
+  await expect(page.getByTestId('ingredients')).toBeVisible();
+  await page.getByRole('link', { name: 'Личный кабинет' }).click();
+  await page.getByRole('link', { name: 'Восстановить пароль' }).click();
+  await page.locator('input[name="email"]').fill('user@example.com');
+  await page.getByRole('button', { name: 'Восстановить' }).click();
+  await page.locator('input[name="password"]').fill('secretpassword');
+  await page.locator('input[name="token"]').fill('tokenFromEmail');
+  await page.getByRole('button', { name: 'Сохранить' }).click();
+  await page.locator('input[name="email"]').fill('user@example.com');
+  await page.locator('input[name="password"]').fill('secretpassword');
+  await page.getByRole('button', { name: 'Войти' }).click();
+  await expect(page.getByRole('link', { name: 'John Doe' })).toBeVisible();
 });
